adac7e1508329ed046d53d1ff00efa5f83db07bd
Dave Mayo <pobocks@gmail.com>
2024-02-23 21:12:51 -0500

    Fix nil error?

--- fixes/unittitle-2.rb
+++ fixes/unittitle-2.rb
@@ -6,10 +6,15 @@ fix_for 'unittitle-2', depends_on: %w|noempty-1 noempty-2| do
     end
     unittitles_by_id[utitle['id']] = utitle
   end
-  @xml.xpath("//ref").each do |ref|
-    if unittitles_by_id.key? ref['target']
+  @xml.xpath("//ref[@target]").each do |ref|
+    if ut = unittitles_by_id[ref['target']]
       # rewrite ref to target enclosing 'c' instead
-      ref['target'] = unittitles_by_id[ref['target']].ancestors('c').first['id']
+      ancestor_cs = ut.xpath("./ancestor::*[contains('c c01 c02 c03 c04 c05 c06 c06 c08 c09 c10 c11 c12', local-name()) and @id]")
+      if ancestor_cs.count > 0
+        ref['target'] = ancestor_cs.last['id']
+      else
+        raise Fixes::Failure.new("No c with id to target")
+      end
     end
   end
 end
